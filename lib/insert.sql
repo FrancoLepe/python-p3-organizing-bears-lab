854030dab153b2b5349614c36af730706ff4bc93
Franco Lepe <Francolepe169@gmail.com>
2023-04-07 09:37:38 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate",5 ,"M", "Grizzly", "mean", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",8 ,"M", "polar", "mean", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",1 ,"F", "black", "nice", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",4 ,"M", "Grizzly", "mean", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",2 ,"F", "Grizzly", "mean", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",10 ,"M", "red GRizzly", "mean", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",12 ,"F", "Grizzly", "mean", 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,3 ,"M", "black", "mean", 0);
